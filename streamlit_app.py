aa13699b646834143159643a04ab307859a6f9cc
jwitkowska <131001238+jwitkowska@users.noreply.github.com>
2023-04-24 10:45:19 +0200

    Update streamlit_app.py
    updated section of api resonse

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas as pd
 import requests
 import snowflake.connector
+from urllib.error import URLError
 
 
 streamlit.title('My Parents New Healthy Dinner')
@@ -22,15 +23,20 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+#new section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
-#streamlit.text(fruityvice_response.json())
+try:
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')
+    if not fruit_choice:
+        streamlit.error("Please select a fruit to get information.")
+    else:
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+        fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+        streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+    streamlit.error()
 
-#normalize json repsonse and show as a table
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
+#streamlit.text(fruityvice_response.json())
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
@@ -41,6 +47,6 @@ streamlit.dataframe(my_data_rows)
 
 #allow end user to add a fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
-my_cur.execute(f"INSERT INTO FRUIT_LOAD_LIST (FRUIT_NAME) VALUES ({add_my_fruit})")
+#my_cur.execute(f"INSERT INTO FRUIT_LOAD_LIST (FRUIT_NAME) VALUES ({add_my_fruit})")
 streamlit.write(f"Thanks for adding {add_my_fruit}")
 
